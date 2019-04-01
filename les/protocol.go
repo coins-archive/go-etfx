2bd45229b888c078238dd02a7ffb8de062a58fc7
etfx-dev <etx.network@protonmail.com>
2019-04-01 18:41:10 +0300

    DAO support disabled. Network ID updated in protocol files

--- les/protocol.go
+++ les/protocol.go
@@ -48,7 +48,7 @@ var (
 var ProtocolLengths = map[uint]uint64{lpv1: 15, lpv2: 22}
 
 const (
-	NetworkId          = 1
+	NetworkId          = 6021117
 	ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
 )
 
